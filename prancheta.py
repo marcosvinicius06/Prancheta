6bdb43cd30f9b03c4a64d7805cb16072af11bc75
marcosvinicius06 <mrcosdias35@gmail.com>
2022-12-11 22:23:51 -0300

    Update prancheta.py

--- prancheta.py
+++ prancheta.py
@@ -1,17 +1,18 @@
 import random
 x = ("ate mais")
-print("1 é sim , 2 é não")
-S = 1
-N = 1
-if S > N:
+Sim = 1
+N = 2
+print("1 é sim , 2 é não") 
+if input (Sim = 1):
     input("Voce comecou")
     nomo = input("qual o seu nome?")
     idade = input("qual a sua idade?")
+    print("--------------")
+    print('nome:', nomo)
+    print('idade:', idade)
+    print("seu numero é:", random.randrange(1, 10))
+    print("--------------")
+    print(x)
 else:
     print(x)
-print("--------------")
-print('nome:', nomo)
-print('idade:', idade)
-print("seu numero é:", random.randrange(1, 10))
-print("--------------")
-print(x)
+
